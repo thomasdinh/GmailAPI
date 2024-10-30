9fd20876722292dc81cb21c27ffc13c81a07eb68
thomasdinh <thomas.dinh.work@outlook.de>
2024-10-30 15:15:15 +0100

    removed gmail_edit_draft, added gmail_read_emails

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def auth():
 
 
 
-def gmail_create_draft(creds):
+def gmail_read_emails(creds):
   """Create and insert a draft email.
    Print the returned draft's message and id.
    Returns: Draft object, including draft id and message meta data.
@@ -91,39 +91,31 @@ def gmail_create_draft(creds):
     # create gmail api client
     service = build("gmail", "v1", credentials=creds)
 
-    message = EmailMessage()
+    # Get a list of message IDs
+    results = service.users().messages().list(userId='me', maxResults=10).execute()
+    messages = results.get('messages', [])
 
-    message.set_content("This is automated draft mail")
+    if not messages:
+        print('No messages found.')
 
-    message["To"] = "dinhthomtest@gmail.com"
-    message["From"] = "peter.dung.hoang@gmail.com"
-    message["Subject"] = "Automated draft"
+    else:
+        print('Message IDs:')
+        for message in messages:
+            print(message['id'])
 
-    # encoded message
-    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-    create_message = {"message": {"raw": encoded_message}}
-    # pylint: disable=E1101
-    draft = (
-        service.users()
-        .drafts()
-        .create(userId="me", body=create_message)
-        .execute()
-    )
-
-    print(f'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
 
   except HttpError as error:
     print(f"An error occurred: {error}")
     draft = None
 
-  return draft
+
 
 
 def main():
     #token
     credentials = auth()
-    gmail_create_draft(credentials)
+    gmail_read_emails(credentials)
     print("Hello!")
 
 
